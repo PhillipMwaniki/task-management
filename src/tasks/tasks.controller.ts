dd20d07da0a331b4af8026630a36d6b922c66045
Phillip Mwaniki <mwanikiphillip@gmail.com>
2021-01-13 00:28:23 +0300

    added class-validator and class-transformer

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,5 +1,7 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
 import { CreateTaskDto } from './dto/create-task.dto';
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
+import { Task, TaskStatus } from './task.model';
 import { TasksService } from './tasks.service';
 
 @Controller('tasks')
@@ -7,17 +9,32 @@ export class TasksController {
     constructor(private tasksService: TasksService) { }
 
     @Get()
-    getAllTasks() {
-        return this.tasksService.getAllTasks();
+    getTasks(@Query() filterDto: GetTasksFilterDto): Task[] {
+        if (Object.keys(filterDto).length) {
+            return this.tasksService.getTasksWithFilters(filterDto);
+        } else {
+            return this.tasksService.getAllTasks();
+        }
+
     }
 
     @Get('/:id')
-    getTaskById(@Param('id') id: string) {
+    getTaskById(@Param('id') id: string): Task {
         return this.tasksService.getTaskById(id);
     }
 
     @Post()
-    createTask(@Body() createTaskDto: CreateTaskDto) {
+    createTask(@Body() createTaskDto: CreateTaskDto): Task {
         return this.tasksService.createTask(createTaskDto);
+    }
+
+    @Patch('/:id/status')
+    updateTask(@Body('status') status: TaskStatus, @Param('id') id: string): Task {
+        return this.tasksService.updateTask(id, status);
+    }
+
+    @Delete('/:id')
+    deleteTaskById(@Param('id') id: string): void {
+        return this.tasksService.deleteTaskById(id);
     }
 }
