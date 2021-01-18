1621d07b1778b37ddda184823853ce30c9e35051
Phillip Mwaniki <mwanikiphillip@gmail.com>
2021-01-18 20:13:12 +0300

    added endpoint to sign in a user

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { UserRepository } from './user.repository';
@@ -11,7 +11,15 @@ export class AuthService {
         private userRepository: UserRepository,
     ) { }
 
-    signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
         return this.userRepository.signUp(authCredentialsDto);
+    }
+
+    async signIn(authCredentialsDto: AuthCredentialsDto): Promise<string> {
+        const username = await this.userRepository.validateUserPassword(authCredentialsDto);
+        if (!username) {
+            throw new UnauthorizedException('invalid credentials');
+        }
+        // return username;
     }
 }
